9c754bf26443a3924ba169c157141c65dc9c23a5
David Marcella <marcella2113@gmail.com>
2022-02-21 19:52:32 -0600

    added a /albums/[id] page that displays an albums title and than all its corresponding photos in a grid

---
+++ __tests__/albums.id.spec.tsx
@@ -0,0 +1,183 @@
+import { render, screen } from '@testing-library/react'
+import Album from '../pages/albums/[id]'
+
+const mockJsonPromiseReject = Promise.resolve(undefined)
+const mockFetchPromiseReject = Promise.reject({
+  json: () => mockJsonPromiseReject,
+  ok: false,
+})
+
+const mockJsonPromise = Promise.resolve([
+  {
+    albumId: 2,
+    id: 57,
+    title: 'vero est optio expedita quis ut molestiae',
+    url: 'https://via.placeholder.com/600/95acce',
+    thumbnailUrl: 'https://via.placeholder.com/150/95acce',
+  },
+  {
+    albumId: 2,
+    id: 58,
+    title: 'rem pariatur facere eaque',
+    url: 'https://via.placeholder.com/600/cde4c1',
+    thumbnailUrl: 'https://via.placeholder.com/150/cde4c1',
+  },
+  {
+    albumId: 2,
+    id: 59,
+    title: 'modi totam dolor eaque et ipsum est cupiditate',
+    url: 'https://via.placeholder.com/600/a46a91',
+    thumbnailUrl: 'https://via.placeholder.com/150/a46a91',
+  },
+])
+const mockFetchPromise = Promise.resolve({
+  json: () => mockJsonPromise,
+  ok: true,
+})
+
+const globalRef: any = global
+
+describe('Album ID Tests', () => {
+  it('should display error message when api promise has issues', async () => {
+    globalRef.fetch = jest
+      .fn()
+      .mockImplementationOnce(() => mockFetchPromiseReject)
+
+    render(<Album id={'2'} />)
+
+    const errorMessasge = await screen.findByText(
+      'There was an issue loading the page.'
+    )
+    expect(errorMessasge).toBeInTheDocument()
+  })
+
+  it('should find photos if api calls are successful', async () => {
+    globalRef.fetch = jest
+      .fn()
+      .mockImplementationOnce((url) => {
+        switch (url) {
+          case 'https://jsonplaceholder.typicode.com/albums/2':
+            return Promise.resolve({
+              json: () =>
+                Promise.resolve({
+                  userId: 1,
+                  id: 2,
+                  title: 'sunt qui excepturi placeat culpa',
+                }),
+              ok: true,
+            })
+          default:
+            return Promise.reject(new Error('not found'))
+        }
+      })
+      .mockImplementationOnce((url) => {
+        switch (url) {
+          case 'https://jsonplaceholder.typicode.com/photos?albumId=2':
+            return Promise.resolve({
+              json: () =>
+                Promise.resolve([
+                  {
+                    albumId: 2,
+                    id: 57,
+                    title: 'vero est optio expedita quis ut molestiae',
+                    url: 'https://via.placeholder.com/600/95acce',
+                    thumbnailUrl: 'https://via.placeholder.com/150/95acce',
+                  },
+                  {
+                    albumId: 2,
+                    id: 58,
+                    title: 'rem pariatur facere eaque',
+                    url: 'https://via.placeholder.com/600/cde4c1',
+                    thumbnailUrl: 'https://via.placeholder.com/150/cde4c1',
+                  },
+                  {
+                    albumId: 2,
+                    id: 59,
+                    title: 'modi totam dolor eaque et ipsum est cupiditate',
+                    url: 'https://via.placeholder.com/600/a46a91',
+                    thumbnailUrl: 'https://via.placeholder.com/150/a46a91',
+                  },
+                ]),
+              ok: true,
+            })
+          default:
+            return Promise.reject(new Error('not found'))
+        }
+      })
+
+    render(<Album id={'2'} />)
+
+    const firstPhoto = await screen.findByText(
+      'vero est optio expedita quis ut molestiae'
+    )
+    expect(firstPhoto).toBeInTheDocument()
+
+    const secondPhoto = await screen.findByText('rem pariatur facere eaque')
+    expect(secondPhoto).toBeInTheDocument()
+
+    const thirdPhoto = await screen.findByText(
+      'modi totam dolor eaque et ipsum est cupiditate'
+    )
+    expect(thirdPhoto).toBeInTheDocument()
+  })
+
+  // it('should display loader skeletons when apis are fetching data', async () => {
+  //   globalRef.fetch = jest
+  //     .fn()
+  //     .mockImplementationOnce((url) => {
+  //       switch (url) {
+  //         case 'https://jsonplaceholder.typicode.com/albums/2':
+  //           return Promise.resolve({
+  //             json: () =>
+  //               Promise.resolve({
+  //                 userId: 1,
+  //                 id: 2,
+  //                 title: 'sunt qui excepturi placeat culpa',
+  //               }),
+  //             ok: true,
+  //           })
+  //         default:
+  //           return Promise.reject(new Error('not found'))
+  //       }
+  //     })
+  //     .mockImplementationOnce((url) => {
+  //       switch (url) {
+  //         case 'https://jsonplaceholder.typicode.com/photos?albumId=2':
+  //           return Promise.resolve({
+  //             json: () =>
+  //               Promise.resolve([
+  //                 {
+  //                   albumId: 2,
+  //                   id: 57,
+  //                   title: 'vero est optio expedita quis ut molestiae',
+  //                   url: 'https://via.placeholder.com/600/95acce',
+  //                   thumbnailUrl: 'https://via.placeholder.com/150/95acce',
+  //                 },
+  //                 {
+  //                   albumId: 2,
+  //                   id: 58,
+  //                   title: 'rem pariatur facere eaque',
+  //                   url: 'https://via.placeholder.com/600/cde4c1',
+  //                   thumbnailUrl: 'https://via.placeholder.com/150/cde4c1',
+  //                 },
+  //                 {
+  //                   albumId: 2,
+  //                   id: 59,
+  //                   title: 'modi totam dolor eaque et ipsum est cupiditate',
+  //                   url: 'https://via.placeholder.com/600/a46a91',
+  //                   thumbnailUrl: 'https://via.placeholder.com/150/a46a91',
+  //                 },
+  //               ]),
+  //             ok: true,
+  //           })
+  //         default:
+  //           return Promise.reject(new Error('not found'))
+  //       }
+  //     })
+
+  //   render(<Album id={'2'} />)
+
+  //   const loaderSkeleton = screen.getByTestId('skeleton-loader')
+  //   expect(loaderSkeleton).toBeInTheDocument()
+  // })
+})
---
+++ pages/albums/[id]/index.tsx
@@ -0,0 +1,103 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { useEffect, useState } from 'react'
+import { ErrorMessage } from '../../../components/error-message/error-message'
+import { PhotoSkeleton } from '../../../components/photo-skeleton/photo-skeleton'
+import { IAlbum } from '../../../types/album'
+import { IPhoto } from '../../../types/photo'
+import { IStatus } from '../../../types/status'
+
+export const Album = ({
+  id,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const [photos, setPhotos] = useState<IPhoto[]>([])
+  const [status, setStatus] = useState<IStatus>('loading')
+  const [album, setAlbum] = useState<IAlbum>({
+    title: null,
+    id: null,
+    userId: null,
+  })
+
+  useEffect(() => {
+    setStatus('loading')
+
+    fetch(`https://jsonplaceholder.typicode.com/albums/${id}`)
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json()
+        } else {
+          throw new Error('Something went wrong')
+        }
+      })
+      .then((album: IAlbum) => {
+        setAlbum(album)
+        return fetch(
+          `https://jsonplaceholder.typicode.com/photos?albumId=${id}`
+        )
+          .then((resp) => {
+            if (resp.ok) {
+              return resp.json()
+            } else {
+              throw new Error('Something went wrong')
+            }
+          })
+          .then((photos: IPhoto[]) => {
+            setPhotos(photos)
+            setStatus('success')
+          })
+      })
+      .catch(() => {
+        setStatus('error')
+      })
+  }, [])
+
+  return (
+    <>
+      {status === 'error' && <ErrorMessage></ErrorMessage>}
+      {status === 'loading' && (
+        <>
+          <div className="p-3 bg-white">
+            <div className="h-8 bg-gray-200 rounded animate-pulse"></div>
+          </div>
+          <div
+            data-testid="loader"
+            className="space-y-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-5"
+          >
+            {[...Array(10).keys()].map((_, index) => {
+              return <PhotoSkeleton key={index}></PhotoSkeleton>
+            })}
+          </div>
+        </>
+      )}
+
+      {status === 'success' && (
+        <>
+          <h1 className="text-4xl">{album.title}</h1>
+
+          <div className="space-y-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-5">
+            {photos.map((photo) => {
+              return (
+                <div
+                  className="rounded overflow-hidden shadow-lg hover:opacity-75 hover:bg-slate-300"
+                  key={photo.id}
+                >
+                  <img className="w-full" src={photo.url} alt={photo.title} />
+                  <div className="flex justify-between px-6 py-4">
+                    <div className="font-bold text-xl">{photo.title}</div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </>
+      )}
+    </>
+  )
+}
+
+export default Album
+
+export const getServerSideProps: GetServerSideProps<{
+  id: string | string[]
+}> = async (context) => {
+  return { props: { id: context.params.id } }
+}
